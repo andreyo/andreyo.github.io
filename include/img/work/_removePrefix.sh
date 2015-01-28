5fac8ed8fd267d8b49eea8015bda1609b97cd65f
andreyo <aosipov.me@gmail.com>
2015-01-27 23:11:21 -0800

    Mustache : photo-content autogeneration

--- include/img/work/_removePrefix.sh
+++ include/img/work/_removePrefix.sh
@@ -1,6 +1,7 @@
-for file in `ls -1 |grep '2015'|awk -F. '{print $1}'`
+for file in `ls -1 |grep '20'|grep -v min|sort -r|awk -F. '{print $1}'`
 do
-    echo mv -v "$file".jpg preg_"${file#*_}".jpg
+    echo \""$file"\",
+    #echo mv -v "$file".jpg "${file%20140909*}201409${file#*20140909}".jpg
     #echo mv -v "$file".jpg preg_"${file%_*}".jpg
     # more on parameter expansion http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
 done
