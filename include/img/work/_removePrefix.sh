4be88f90dbabd2ac03057031d3d8cbc3fdb719c1
andreyo <aosipov.me@gmail.com>
2015-12-04 21:00:49 -0800

    201511

--- include/img/work/_removePrefix.sh
+++ include/img/work/_removePrefix.sh
@@ -1,7 +1,7 @@
-for file in `ls -1 |grep '201510'|grep -v min|sort -r|awk -F. '{print $1}'`
+for file in `ls -1 |grep 'DSC'|sort -r|awk -F. '{print $1}'`
 do
-    echo \""$file"\",
-    #echo mv -v "$file".jpg "${file%20140909*}201409${file#*20140909}".jpg
-    #echo mv -v "$file".jpg preg_"${file%_*}".jpg
+    #echo \""$file"\",
+    mv -v "$file".jpg wed_"${file%DSC*}201511${file#*DSC}".jpg
+    #echo mv -v "$file".jpg fam_"${file%_*}".jpg
     # more on parameter expansion http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
 done
