64ca2f29470164cdf1cd9863295b4a59ead8a906
andreyo <aosipov.me@gmail.com>
2016-02-03 22:08:09 -0800

    201602

--- include/img/work/_removePrefix.sh
+++ include/img/work/_removePrefix.sh
@@ -1,7 +1,7 @@
 for file in `ls -1 |grep 'DSC'|sort -r|awk -F. '{print $1}'`
 do
     #echo \""$file"\",
-    #mv -v "$file".jpg wed_"${file%DSC*}201511${file#*DSC}".jpg
+    mv -v "$file".jpg pers_"${file%DSC*}201602${file#*DSC}".jpg
     #echo mv -v "$file".jpg fam_"${file%_*}".jpg
     # more on parameter expansion http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_06_02
 done
